cbea153b4b880997874c4909da9bd520a18026a8
Small_Orangeee <3314500052@qq.com>
2025-09-23 23:54:58 +0800

    使用premake作为构建工具接管工程

---
+++ premake5.lua
@@ -0,0 +1,69 @@
+workspace "Yuicy"
+    architecture "x64"
+    startproject "Sandbox"
+    configurations { "Debug", "Release" }
+
+outputdir = "%{cfg.buildcfg}-x64"
+rundir    = "%{wks.location}bin\\" .. outputdir .. "\\Sandbox"
+
+group "Dependencies"
+    include "Yuicy/thirdparty/GLFW"
+
+group ""
+project "Yuicy"
+    location "Yuicy"
+    kind "SharedLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "Off"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir    ("bin/int/" .. outputdir .. "/%{prj.name}")
+    pchheader "pch.h"
+    pchsource "Yuicy/src/pch.cpp"
+    files { "Yuicy/src/**.h", "Yuicy/src/**.hpp", "Yuicy/src/**.cpp" }
+    includedirs { "Yuicy/src", "Yuicy/thirdparty/spdlog/include", "Yuicy/thirdparty/GLFW/include" }
+    defines { "PLATFORM_WINDOWS", "YUICY_EXPORT_DLL" }
+    filter "system:windows"
+        systemversion "latest"
+        buildoptions { "/utf-8" }
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+    filter {}
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "Off"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir    ("bin/int/" .. outputdir .. "/%{prj.name}")
+    files { "Sandbox/src/**.h", "Sandbox/src/**.hpp", "Sandbox/src/**.cpp" }
+    includedirs { "Yuicy/src", "Yuicy/thirdparty/spdlog/include", "Yuicy/thirdparty/GLFW/include" }
+    links { "Yuicy", "GLFW" }
+    defines { "PLATFORM_WINDOWS" }
+    filter "system:windows"
+        systemversion "latest"
+        buildoptions { "/utf-8" }
+        links { "opengl32", "user32", "gdi32", "shell32" }
+        postbuildcommands {
+			'cmd /c if not exist "%{cfg.targetdir}" mkdir "%{cfg.targetdir}"',
+			-- library
+			'cmd /c if exist "%{wks.location}bin\\%{cfg.buildcfg}-x64\\Yuicy\\*.dll" copy /Y "%{wks.location}bin\\%{cfg.buildcfg}-x64\\Yuicy\\*.dll" "%{cfg.targetdir}"',
+			'cmd /c if exist "%{wks.location}Yuicy\\thirdparty\\GLFW\\bin\\%{cfg.buildcfg}-x64\\GLFW\\*.lib" copy /Y "%{wks.location}Yuicy\\thirdparty\\GLFW\\bin\\%{cfg.buildcfg}-x64\\GLFW\\*.lib" "%{cfg.targetdir}"',
+			-- pdb
+			'cmd /c if exist "%{wks.location}bin\\%{cfg.buildcfg}-x64\\Yuicy\\*.pdb" copy /Y "%{wks.location}bin\\%{cfg.buildcfg}-x64\\Yuicy\\*.pdb" "%{cfg.targetdir}"',
+			'cmd /c if exist "%{wks.location}Yuicy\\thirdparty\\GLFW\\bin\\%{cfg.buildcfg}-x64\\GLFW\\*.pdb" copy /Y "%{wks.location}Yuicy\\thirdparty\\GLFW\\bin\\%{cfg.buildcfg}-x64\\GLFW\\*.pdb" "%{cfg.targetdir}"'
+
+        }
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "On"
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "On"
+    filter {}
